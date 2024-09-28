b541d2ef83e9f6c676ebda3f126b893b9f6849bf
Abdurashid <a.jurayev@uic.group>
2024-09-28 15:01:23 +0500

    card changes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const tham = document.querySelector(".tham");
 const nav = document.querySelector("nav");
+const taskTitle = document.querySelector('.task-title')
+const cardTask = document.querySelector('.card-task')
 tham.addEventListener("click", () => {
   tham.classList.toggle("tham-active");
   nav.classList.toggle("open");
 });
+taskTitle.addEventListener('click',()=>{
+  
+})
