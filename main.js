0e43060560098208567868c7e73238fd51533413
Abdurashid <a.jurayev@uic.group>
2024-09-30 00:14:57 +0500

    fix card

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const tham = document.querySelector(".tham");
 const nav = document.querySelector("nav");
-const taskTitle = document.querySelector('.task-title')
-const cardTask = document.querySelector('.card-task')
+const cardCourse = document.querySelector(".card-course");
+const taskTitle = document.querySelector(".task-title");
+const cardWrap = document.querySelector(".card-wrap");
 tham.addEventListener("click", () => {
   nav.classList.toggle("open");
   tham.classList.toggle("tham-active");
 });
-taskTitle.addEventListener('click',()=>{
-  
-})
+taskTitle.addEventListener("click", () => {
+  cardWrap.classList.toggle("open-card");
+});
