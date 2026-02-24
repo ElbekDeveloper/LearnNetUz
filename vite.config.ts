de2ff0200391cdcda505adbbdc49afaca5771062
Elbek <elbek.normurodov@smartbank.uz>
2026-02-25 00:01:15 +0500

    Update Latest

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/LearnNetUz/", 
   plugins: [
     react(),
     tailwindcss(),
