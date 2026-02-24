019e63e46a526f796f07bc30002b4f1980b1ecae
Elbek <elbek.normurodov@smartbank.uz>
2026-02-24 23:11:38 +0500

    Add Base Address

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/LearnNetUz/", 
   plugins: [
     react(),
     tailwindcss(),
